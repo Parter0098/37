543b74941e8cc38a2fd0991585993322281f3274
Saman <samanjan1398@gmail.com>
2024-11-30 13:48:16 +0330

    add

---
+++ index.js
@@ -0,0 +1,15 @@
+let firsName = 'Saman'
+let lastName = 'Jan Jan'
+let age = 19
+let FatherName = 'Hadi'
+
+console.log(
+    'my first name is ' +
+    firsName +
+    ' and my last name is ' +
+    lastName +
+    ' as i say my age is ' +
+    age +
+    ' and my Father name is ' +
+    FatherName
+)
